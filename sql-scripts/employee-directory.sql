63b7f64c9a5bc4db8aca892f1fb2c4dd11a5991b
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-01-31 16:04:33 -0300

    Changing of database creation script to PostgreSQL sintax.

--- sql-scripts/employee-directory.sql
+++ sql-scripts/employee-directory.sql
@@ -1,28 +1,27 @@
-CREATE DATABASE  IF NOT EXISTS `employee_directory`;
-USE `employee_directory`;
+CREATE DATABASE employee_directory;
+\c employee_directory;
 
 --
--- Table structure for table `employee`
+-- Table structure for table employee
 --
 
-DROP TABLE IF EXISTS `employee`;
-
-CREATE TABLE `employee` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+CREATE TABLE employee (
+  id serial NOT NULL,
+  first_name varchar(45) NULL,
+  last_name varchar(45) NULL,
+  email varchar(45) NULL,
+  CONSTRAINT employee_pkey PRIMARY KEY (id)
+);
 
 --
--- Data for table `employee`
+-- Data for table employee
 --
 
-INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
-
+INSERT INTO employee
+	(first_name, last_name, email)
+	VALUES 
+	('Leslie','Andrews','leslie@luv2code.com'),
+	('Emma','Baumgarten','emma@luv2code.com'),
+	('Avani','Gupta','avani@luv2code.com'),
+	('Yuri','Petrov','yuri@luv2code.com'),
+	('Juan','Vega','juan@luv2code.com');
