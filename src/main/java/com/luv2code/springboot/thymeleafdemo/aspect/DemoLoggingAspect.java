9747effcd8006a5fb17efc9f2e04422b43974c30
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-15 13:31:10 -0300

    Logging executed method arguments on @Before advice method from aspect class. Adding another advice method to log executed method signature and result data by using @AfterReturning annotation.

--- src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.thymeleafdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -37,5 +38,26 @@ public class DemoLoggingAspect {
         myLogger.info("=====>> in @Before: calling method: " + theMethod);
 
         // display the arguments to the method
+
+        // get the arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru and display args
+        for (Object tempArg : args) {
+            myLogger.info("=====>> argument: " + tempArg);
+        }
+    }
+
+    @AfterReturning(
+        pointcut = "forAppFlow()",
+        returning = "theResult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====>> in @AfterReturning: calling method: " + theMethod);
+
+        // display data returned
+        myLogger.info(("=====>> result: " + theResult));
     }
 }
